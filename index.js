18de4bb57d753dbdc47cd2c2b29a96de53d470f8
Corey Kress <coreykre@buffalo.edu>
2019-03-08 20:37:23 -0500

    fix stupid thing

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.broadcast.emit('hi');
-
+  // socket.broadcast.emit('user', 'New User Joined!');
+  io.emit('user', 'New User Joined');
   console.log('user connected');
+
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log('message: ' + msg);
